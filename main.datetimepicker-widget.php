48d082b5f49980927e984966199e02b1b8172fab
Vladimir Kunin <v.b.kunin@gmail.com>
2014-12-25 12:41:25 +0300

    Initial Commit

---
+++ main.datetimepicker-widget.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Module DateTimePicker Wigdet (jQuery plugin)
+ * https://github.com/vbkunin/datetimepicker-widget
+ *
+ * @author      Vladimir Kunin <v.b.kunin@gmail.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+class DateTimePickerJQueryPlugIn implements iPageUIExtension
+{
+
+  public function GetBannerHtml(iTopWebPage $oPage)
+  {
+    $sConfJSON = json_encode(utils::GetConfig()->GetModuleSetting('datetimepicker-widget', 'default'));
+    $oPage->add_linked_stylesheet(utils::GetAbsoluteUrlModulesRoot().'datetimepicker-widget/css/jquery.datetimepicker.css');
+    $oPage->add_linked_script(utils::GetAbsoluteUrlModulesRoot().'datetimepicker-widget/js/jquery.datetimepicker.js');
+    $oPage->add_ready_script(
+<<< EOF
+      var config = $sConfJSON;
+      try {
+        $(".datetime-pick, .date-pick")
+          .datepicker("destroy")
+          .each(function () {
+            var img = document.createElement('img');
+            img.className = "calendar";
+            img.name = this.name;
+            img.src = "../images/calendar.png";
+            $(this).after(img);
+            if (this.className == 'datetime-pick') {
+              config.format = 'Y-m-d H:i';
+            } else if (this.className == 'date-pick') {
+              config.format = 'Y-m-d';
+              config.timepicker = false;
+              config.closeOnDateSelect = true;
+            }
+          })
+          .datetimepicker(config);
+        $('.calendar').click(function () {
+          $(".datetime-pick[name='" + this.name + "'], .date-pick[name='" + this.name + "']").datetimepicker('show');
+        });
+      } catch (err) {
+        // console.log(err);
+      }
+EOF
+    );
+  }
+
+  public function GetNorthPaneHtml(iTopWebPage $oPage)
+  {
+  }
+
+  public function GetSouthPaneHtml(iTopWebPage $oPage)
+  {
+  }
+
+}
+?>
---
+++ module.datetimepicker-widget.php
@@ -0,0 +1,56 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'datetimepicker-widget/',
+	array(
+		// Identification
+		//
+		'label' => 'DateTimePicker Widget',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'main.datetimepicker-widget.php'
+		),
+		'webservice' => array(
+
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+
+		// Documentation
+		//
+		'doc.manual_setup' => 'https://github.com/vbkunin/datetimepicker-widget', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => 'https://github.com/vbkunin/datetimepicker-widget', // hyperlink to more information, if any
+
+		// Default settings
+		//
+		'settings' => array(
+			'default' => array(
+				'lang' => 'ru',
+				'dayOfWeekStart' => 1,
+				'step' => 30
+				)
+		),
+	)
+);
+
+
+?>
