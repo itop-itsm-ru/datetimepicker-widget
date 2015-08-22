fdae1acf1c9a99c976e5b29140a92b91c3390846
Vladimir Kunin <v.b.kunin@gmail.com>
2015-08-23 00:19:14 +0300

    Git info

--- module.datetimepicker-widget.php
+++ module.datetimepicker-widget.php
@@ -3,13 +3,36 @@
 // iTop module definition file
 //
 
+// Git commit info
+if (file_exists(dirname(__FILE__).'/.git/HEAD'))
+{
+	$sDate = date("d.m.y G:i", filemtime(dirname(__FILE__).'/.git/index'));
+	$sHead = trim(preg_replace('/ref:/', '', file_get_contents(dirname(__FILE__).'/.git/HEAD'), -1, $iCount));
+	if ($iCount === 1)
+	{
+		$sCommit = '(commit '.file_get_contents(dirname(__FILE__).'/.git/'.$sHead, false, null, 0, 7).' from '.$sDate.')';
+	}
+	elseif ($iCount === 0)
+	{
+		$sCommit = '(commit '.substr($sHead, 0, 7).' from '.$sDate.')';
+	}
+	else
+	{
+		$sCommit = '(commit info not found)';
+	}
+}
+else
+{
+	$sCommit = '';
+}
+
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
 	'datetimepicker-widget/',
 	array(
 		// Identification
 		//
-		'label' => 'DateTimePicker Widget',
+		'label' => "DateTimePicker Widget $sCommit",
 		'category' => 'business',
 
 		// Setup
@@ -37,7 +60,7 @@ SetupWebPage::AddModule(
 
 		// Documentation
 		//
-		'doc.manual_setup' => 'extensions/datetimepicker-widget/doc/readme.html', // hyperlink to manual setup documentation, if any
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
 		'doc.more_information' => 'extensions/datetimepicker-widget/doc/readme.html', // hyperlink to more information, if any
 
 		// Default settings
